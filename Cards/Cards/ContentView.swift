1e61154126e9125dbf57b7706bcc6b2cebca9fd0
정채은 <cce523@hanmail.net>
2023-03-10 12:13:55 +0900

    [Feat] CardView

--- Cards/Cards/ContentView.swift
+++ Cards/Cards/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List{
+            PersonCardView(person: person1)
+            PersonCardView(person: person2)
         }
-        .padding()
+        .listStyle(.plain)
     }
 }
 
