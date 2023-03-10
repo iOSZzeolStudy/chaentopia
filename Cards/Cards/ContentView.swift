687eac13efa21a7221a8ca6a8f1ac231857c61df
정채은 <cce523@hanmail.net>
2023-03-10 11:39:26 +0900

    [Add] Cards Project

---
+++ Cards/Cards/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cards
+//
+//  Created by 정채은 on 2023/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
