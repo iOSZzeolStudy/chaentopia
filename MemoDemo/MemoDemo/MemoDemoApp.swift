994c62e0e578a38842eee1e7f36c3815adab9fdf
정채은 <cce523@hanmail.net>
2023-03-17 12:15:13 +0900

    [Setting] 프로젝트 세팅

---
+++ MemoDemo/MemoDemo/MemoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoDemoApp.swift
+//  MemoDemo
+//
+//  Created by 정채은 on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct MemoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
