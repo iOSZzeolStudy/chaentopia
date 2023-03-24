14566afc8e57584edd3158b7f5f219bfedc48cc3
정채은 <cce523@hanmail.net>
2023-03-24 16:23:59 +0900

    [Feat] UI 생성

--- MemoDemo/MemoDemo/ContentView.swift
+++ MemoDemo/MemoDemo/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var title: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("메모")
+                .font(.title).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(5)
+            TextField("검색", text: $title)
+                .textFieldStyle(.roundedBorder)
+                .padding(5)
+            Spacer()
+            Group {
+                Text("어제")
+                    .font(.title2).bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                VStack(alignment: .leading ,spacing: 1) {
+                    MemoView()
+                    MemoView()
+                    MemoView()
+                }
+                .cornerRadius(10)
+                Spacer()
+            }
+            Group {
+                Text("이전 7일")
+                    .font(.title2).bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                VStack(alignment: .leading ,spacing: 1) {
+                    MemoView()
+                    MemoView()
+                    MemoView()
+//                    MemoView()
+                }
+                .cornerRadius(10)
+                Spacer()
+            }
         }
         .padding()
+        .background(Color(hue: 0.65, saturation: 0.03, brightness: 0.95))
     }
 }
 
