08269ae2ba3447525674ca4e5e76262a7d86b07f
정채은 <cce523@hanmail.net>
2023-03-31 20:28:05 +0900

    [Feat] 메모 쓰기 화면

---
+++ SwiftUIMemo/SwiftUIMemo/View/ComposeView.swift
@@ -0,0 +1,54 @@
+//
+//  ComposeView.swift
+//  SwiftUIMemo
+//
+//  Created by 정채은 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: MemoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("새 메모")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading)
+                {
+                    Button{
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing)
+                {
+                    Button{
+                        store.insert(memo: content)
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(MemoStore())
+    }
+}
