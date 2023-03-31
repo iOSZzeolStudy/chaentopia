f029f4f0945786728375698b63ab7f309c81023f
정채은 <cce523@hanmail.net>
2023-03-31 18:16:38 +0900

    [Feat] 메모 목록 화면 UI

--- SwiftUIMemo/SwiftUIMemo/MainListView.swift
+++ SwiftUIMemo/SwiftUIMemo/MainListView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store : MemoStore
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .navigationTitle("메모")
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
--- SwiftUIMemo/SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemo/SwiftUIMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
